75b7836d962d89ca1d7b6690624f2bccb5ea0992
Beatrix Vad <beatrix.vad@egym.de>
2017-10-31 16:13:01 +0100

    Json unmarshalling of input wichtel

--- wichtelbot.go
+++ wichtelbot.go
@@ -1,25 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"encoding/json"
+)
 
 // A wichtel for the wichtel draw - wichtel are identified by their email
 type Wichtel struct {
-	Name  string
-	Email string
-	ExcludeWichtel []Wichtel
+	Name                  string
+	Email                 string
+	ExcludeWichtelByEmail []string
 }
+
+type WichtelGroup []Wichtel
 
 // Main function running the wichtel draw
 func main() {
 	fmt.Println("Hello wichtel admin!")
+
 }
 
 // Draw a random wichtel from the wichtel pool
-func drawRandomWichtel() {
+func (wichtelOut WichtelGroup) drawRandomWichtel(wichtelIn WichtelGroup) {
 
 }
 
 // Send out wichtel emails
 func sendEmail() {
 
+}
+
+func (wichtelGroup *WichtelGroup) readWichtelInput() {
+
+	jsonInput := []byte(`[{
+		  "Name": "Test Wichtel 1",
+		  "Email": "testwichtel1@test.de"
+		}]`);
+
+	err := json.Unmarshal(jsonInput, wichtelGroup)
+
+	if (err != nil) {
+		fmt.Println("Wichtel unmarshal failed.", err)
+	}
 }
--- wichtelbot_test.go
+++ wichtelbot_test.go
@@ -1,19 +1,32 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestWichtel(t *testing.T) {
 
 	// construct new wichtel
-	wichtel1 := Wichtel{"Test Wichtel", "testmail@test.de", []Wichtel{}}
+	wichtel1 := Wichtel{"Test Wichtel", "testmail@test.de", []string{}}
 
 	wichtelName := wichtel1.Name
-	if(wichtelName) != "Test Wichtel"{
+	if (wichtelName) != "Test Wichtel" {
 		t.Error("Expected Test Wichtel, got ", wichtelName)
 	}
 
 	wichtelEmail := wichtel1.Email
-	if(wichtelEmail) != "testmail@test.de"{
+	if (wichtelEmail) != "testmail@test.de" {
 		t.Error("Expected testmail@test.de, got ", wichtelEmail)
+	}
+}
+
+func TestWichtelJson(t *testing.T) {
+
+	var wichtelGroup WichtelGroup
+
+	wichtelGroup.readWichtelInput()
+
+	if (len(wichtelGroup) != 1) {
+		t.Error("Expected 1 wichtel, got ", wichtelGroup)
 	}
 }
